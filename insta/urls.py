d9996c4371eee6e8ada0a06a12ef36617b2fca71
ANGELA <angela.mbithe@student.moringaschool.com>
2021-07-10 17:00:26 +0300

    add nav bar

--- insta/urls.py
+++ insta/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-path('', views.welcome , name='index') 
+   path('', views.welcome , name='index'),
+   path('comment/<id>',views.comments,name="comments")
+
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- insta/views.py
+++ insta/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Post, Profile, Comment, Follow
 
 # Create your views here.
 def welcome(request):
-    
-    return render(request, 'index.html')
+    posts=Post.objects.all()
+    return render(request, 'index.html',  {"posts":posts})
+
+def comments(request,id):
+    all_comments = Comment.get_comments(id)
+    return render(request, 'comments.html', {"comments":all_comments})
