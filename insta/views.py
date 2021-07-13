60cdad894b1d61bed5a41a0990cfbe0f0a62454f
ANGELA <angela.mbithe@student.moringaschool.com>
2021-07-13 15:51:35 +0300

    add redirect to login

--- insta/views.py
+++ insta/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import redirect, render
 from django.http  import HttpResponse
 from .models import Post, Profile, Comment, Follow
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -13,13 +16,30 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')
+            return redirect('login') 
+        name = form.cleaned_data.get("username")
+        messages.success(request,name+"'s account successfully created")
     context = {'form':form}
     return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Incorrect Username or Password')
     context = {}
     return render(request, 'accounts/login.html', context)
+
+def logoutpage(request):
+    logout(request)
+    return redirect('login')
 
 def welcome(request):
     posts=Post.objects.all()
