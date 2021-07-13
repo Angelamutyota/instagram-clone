9a9383a781de73d0687f7a680d13abd25707e6f1
ANGELA <angela.mbithe@student.moringaschool.com>
2021-07-13 14:58:49 +0300

    add profile photo to user profile

--- insta/forms.py
+++ insta/forms.py
@@ -1,7 +1,20 @@
-from .models import Post
+from . models import Post,Profile
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class UploadImageForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['image','name','caption']
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
+
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_photo', 'bio']
--- insta/models.py
+++ insta/models.py
@@ -5,6 +5,7 @@ from tinymce.models import HTMLField
 
 # Create your models here.
 class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'profile/',default='SOME IMAGE')
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True, default='No bio')
     profile = models.ImageField(upload_to='images/', default='')
